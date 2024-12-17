878f4ff4c76c1203d7497186a32a78032631859f
Sumit <tonystark83033@gmail.com>
2024-12-17 17:54:08 +0530

    add jsx type in faq

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,9 @@ const eslintConfig = [
   rules: {
     "@typescript-eslint/no-empty-object-type": "off",
     "@typescript-eslint/no-explicit-any":"off",
-    "@typescript-eslint/no-unused-vars":"off"
+    "@typescript-eslint/no-unused-vars":"off",
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
   }
 }
   
