15bfc5e25ec173885f832d1f209b4ea772292dad
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2025-06-02 19:14:07 +0530

    updated code

--- src/data/paymentLink.ts
+++ src/data/paymentLink.ts
@@ -7,5 +7,5 @@ export const payment = {
   paymentLink: "https://forms.gle/6r8Qa7UR9TNTXXZG9",
   studentRouteLink: "https://rzp.io/rzp/R36lbP1",
   professionRouteLink: "https://rzp.io/rzp/IqQ41gks",
-  fullstackLink: "http://localhost:3000/fullstack",
+  fullstackLink: "https://rzp.io/rzp/IqQ41gks",
 }
