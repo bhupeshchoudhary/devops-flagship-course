dc2f447e30e540d86f034fc39599bae959891e0f
anshuraghv12 <anshuraghav1204@gmail.com>
2025-06-03 16:52:59 +0530

    update code

--- src/data/paymentLink.ts
+++ src/data/paymentLink.ts
@@ -4,8 +4,8 @@
   // src/data/paymentLink.ts
 
 export const payment = {
-  paymentLink: "https://forms.gle/6r8Qa7UR9TNTXXZG9",
+  paymentLink: "https://forms.gle/MnHbGEi2w31PnLJ48",
   studentRouteLink: "https://rzp.io/rzp/R36lbP1",
   professionRouteLink: "https://rzp.io/rzp/IqQ41gks",
-  fullstackLink: "https://rzp.io/rzp/IqQ41gks",
+  fullstackLink: "https://rzp.io/rzp/Vm0ufqh",
 }
